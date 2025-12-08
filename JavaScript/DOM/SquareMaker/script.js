c76a215ceaaeacee28757da689e4e87e2af76973
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2025-12-08 20:24:46 +0530

    Todo list web app

--- JavaScript/DOM/SquareMaker/script.js
+++ JavaScript/DOM/SquareMaker/script.js
@@ -1,14 +1,29 @@
 const input = document.getElementById("input");
 const button = document.getElementById("button");
 const boxContainer = document.getElementById("boxContainer");
+const colorInput = document.getElementById("colorinput");
+const shapeInput = document.getElementById("shape");
+const textInput = document.getElementById("text");
 
 button.addEventListener("click", function () {
-  const numberOfBoxes = input.value;
-  console.log(numberOfBoxes);
+  console.dir(input);
+  const numberOfBoxes = input.valueAsNumber;
+  const backgroundColor = colorInput.value;
+  const shapeValue = shapeInput.value;
+  const textInputValue = textInput.value;
+  console.log(typeof numberOfBoxes);
+  if (typeof numberOfBoxes !== "number") {
+    alert("Please enter a number");
+    return;
+  }
+
+  //   boxContainer.innerHTML = "";
   for (let i = 1; i <= numberOfBoxes; i++) {
     let box = document.createElement("div");
-    box.classList.add("square");
+
+    box.classList.add(shapeValue);
+    box.style.backgroundColor = backgroundColor;
+    box.innerText = textInputValue;
     boxContainer.appendChild(box);
   }
-  
 });
