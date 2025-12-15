4e6bfef642171edda5a5b35b06778684b5dbe7d6
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2025-12-15 19:48:41 +0530

    Ticket adder all functionality done

--- JavaScript/DOM/TaskManager/script.js
+++ JavaScript/DOM/TaskManager/script.js
@@ -4,10 +4,40 @@ const ticketAdderColorContainer = document.querySelector(
   ".ticket-adde-color-container"
 );
 
-addTicketButton.addEventListener("click", function () {
+const ticketAdderTextArea = document.querySelector(".ticket-adder-text");
+
+const taskAdderColors = document.querySelectorAll(".ticket-adde-color");
+
+let currentTaskAdderSelectedColor = "red";
+
+function toggleTickerAdder() {
   tickerAdderBlock.classList.toggle("hide");
-});
+}
+
+addTicketButton.addEventListener("click", toggleTickerAdder);
 
 ticketAdderColorContainer.addEventListener("click", function (event) {
-  console.log(event);
+  // Selecting the target container
+  const targetContainer = event.target;
+  // from targetContainer getting the color which user has selected
+  currentTaskAdderSelectedColor = targetContainer.classList[1];
+  // removing the border from all the colors
+  for (let i = 0; i < taskAdderColors.length; i++) {
+    let currentContainer = taskAdderColors[i];
+    currentContainer.classList.remove("active-color");
+  }
+  // Adding the border class to target container
+  targetContainer.classList.add("active-color");
+});
+
+ticketAdderTextArea.addEventListener("keydown", function (e) {
+  let keyPressed = e.key;
+  let task = ticketAdderTextArea.value;
+
+  if (keyPressed == "Enter") {
+    // create the ticket ;
+    console.log("task", task, "color", currentTaskAdderSelectedColor);
+    ticketAdderTextArea.value = "";
+    toggleTickerAdder();
+  }
 });
