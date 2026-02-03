1d57f86741ce841a9d44ca5a7f53aacef2407d18
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2026-02-03 19:44:12 +0530

    Node js figlet installed

--- Nodejs/index.js
+++ Nodejs/index.js
@@ -1,3 +1,12 @@
 import { sum, sub, mult } from "./Maths.js";
 
-console.log(sum(2, 3));
+// console.log(sum(2, 3));
+
+import figlet from "figlet";
+
+async function doStuff() {
+  const text = await figlet.text("SAURABH BANSAL");
+  console.log(text);
+}
+
+doStuff();
