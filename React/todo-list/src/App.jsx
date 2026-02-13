269753dc0dfa1d5c801e29270f4f02821ea41ed0
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2026-02-13 19:33:32 +0530

    todo list ended and dynamic search bar started

--- React/todo-list/src/App.jsx
+++ React/todo-list/src/App.jsx
@@ -1,22 +1,61 @@
 import React from "react";
 import "./index.css";
+import { useState } from "react";
 
 const App = () => {
+  let defaultTask = [];
+  const taskFromLocalStorage = localStorage.getItem("todolist-react");
+  if (taskFromLocalStorage) {
+    defaultTask = JSON.parse(taskFromLocalStorage);
+  }
+  const [task, setTask] = useState("");
+  const [taskArray, setTaskArray] = useState(defaultTask);
+  function handleChange(e) {
+    setTask(e.target.value);
+  }
+  function handleClick() {
+    setTaskArray([...taskArray, { id: Date.now(), task }]);
+    localStorage.setItem(
+      "todolist-react",
+      JSON.stringify([...taskArray, { id: Date.now(), task }]),
+    );
+    setTask("");
+  }
+  function handleDelete(id) {
+    const filteredTask = taskArray.filter((taskObj) => taskObj.id != id);
+    localStorage.setItem("todolist-react", JSON.stringify(filteredTask));
+    setTaskArray(filteredTask);
+  }
   return (
     <div className="root">
+      {console.log(taskArray)}
       <div className="inputTextContainer">
-        <input className="input" type="text" />
-        <button className="addBtn">Add</button>
+        <input
+          value={task}
+          onChange={handleChange}
+          className="input"
+          type="text"
+        />
+        <button onClick={handleClick} className="addBtn">
+          Add
+        </button>
       </div>
       <div className="taskContainer">
-        <div className="task">
-          <p>I am task 1 </p>
-          <button className="addBtn deleteBtn">Delete</button>
-        </div>
-         <div className="task">
-          <p>I am task 1 </p>
-          <button className="addBtn deleteBtn">Delete</button>
-        </div>
+        {taskArray.map((taskObj) => {
+          return (
+            <div key={taskObj.id} className="task">
+              <p>{taskObj.task} </p>
+              <button
+                onClick={() => {
+                  handleDelete(taskObj.id);
+                }}
+                className="addBtn deleteBtn"
+              >
+                Delete
+              </button>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
