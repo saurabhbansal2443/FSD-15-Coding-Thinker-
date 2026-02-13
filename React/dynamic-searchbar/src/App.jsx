20eaf85e9847a073d62e460a34eba1d50b2e8f3c
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2026-02-13 20:29:04 +0530

    api fetched

--- React/dynamic-searchbar/src/App.jsx
+++ React/dynamic-searchbar/src/App.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const App = () => {
+  const [query, setQuery] = useState("");
+
+  async function getData() {
+    const trimmedQuery = query.trim(); 
+    if (trimmedQuery.length == 0) return;
+    let apiData = await fetch(
+      `https://dummyjson.com/products/search?q=${trimmedQuery}`,
+    );
+    let jsonData = await apiData.json();
+    console.log(jsonData);
+  }
+
+  useEffect(
+    function () {
+      getData();
+    },
+    [query],
+  );
+
   return (
     <div>
-      
+      {console.log(query)}
+      <input
+        onChange={(e) => {
+          setQuery(e.target.value);
+        }}
+        value={query}
+        type="text"
+      />
+      <div className="suggestionBox"></div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
