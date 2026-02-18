55eb04f1479f4bcc7b0969eb3c4f57a61427d93a
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2026-02-18 19:29:55 +0530

    searchbar completed

--- React/ecommerce/src/Components/Navbar.jsx
+++ React/ecommerce/src/Components/Navbar.jsx
@@ -1,14 +1,12 @@
 import React from "react";
+import SearchBar from "./SearchBar";
 
 const Navbar = () => {
   return (
     <div className="h-12 w-screen border-2 border-blue-300 bg-blue-300 flex items-center justify-around">
       <p className="text-2xl text-white font-extrabold">Shopsy</p>
 
-      <input
-        className="bg-white rounded-2xl w-2/8 h-3/4  focus:outline-none border-none pl-5 pr-5"
-        type="text"
-      />
+      <SearchBar />
 
       <div className="flex justify-between items-center w-1/8">
         <p className="text-xl text-white ">Wishlist</p>
---
+++ React/ecommerce/src/Components/SearchBar.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useRef, useState } from "react";
+
+const SearchBar = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchSuggestion, setSearchSuggestion] = useState([]);
+  const timerId = useRef(null);
+
+  async function getData() {
+    const trimmedQuery = searchQuery.trim();
+    if (trimmedQuery.length == 0) return;
+    const apiData = await fetch(
+      `https://dummyjson.com/products/search?q=${trimmedQuery}`,
+    );
+    const jsonData = await apiData.json();
+    setSearchSuggestion(jsonData.products);
+  }
+
+  useEffect(() => {
+    if (timerId.current) {
+      clearTimeout(timerId.current);
+    }
+    timerId.current = setTimeout(getData, 400);
+  }, [searchQuery]);
+
+  let showSearchSuggestion =
+    searchQuery.trim().length > 0 && searchSuggestion.length > 0;
+
+  return (
+    <div className="w-2/8 h-3/4">
+      <input
+        onChange={(e) => {
+          setSearchQuery(e.target.value);
+        }}
+        className="bg-white w-full h-full rounded-2xl   focus:outline-none border-none pl-5 pr-5"
+        type="text"
+      />
+      {showSearchSuggestion && (
+        <div className="bg-white rounded-xl border-2 border-gray-200 p-3 mt-1">
+          {searchSuggestion.map((pObj) => {
+            return (
+              <div className="bg-gray-100 mb-1 p-2 rounded-xl">
+                <p className="text-sm text-gray-600">{pObj.title}</p>
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SearchBar;
