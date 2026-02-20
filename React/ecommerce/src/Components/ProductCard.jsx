0f20308e25bd4eccbd27616b95e5161ab96ce070
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2026-02-20 19:20:53 +0530

    Prodcut card and icon managment

--- React/ecommerce/src/Components/ProductCard.jsx
+++ React/ecommerce/src/Components/ProductCard.jsx
@@ -1,11 +1,36 @@
 import React from "react";
+import Star from "../Icons/Star";
 
 const ProductCard = ({ data }) => {
-  const { title, price, category, discountPercentage, rating, brand } = data;
+  const {
+    title,
+    price,
+    discountPercentage,
+    rating,
+    brand,
+    thumbnail,
+  } = data;
   return (
-    <div>
-      <p>{title}</p>
-      <p>{price}</p>
+    <div className="w-[20rem] h-[40vh] bg-gray-100 rounded-xl flex  items-center flex-col justify-start  overflow-hidden p-2">
+      <img
+        className="h-[70%] w-full bg-white  rounded-xl"
+        src={thumbnail}
+        alt=""
+      />
+      <div className="w-full">
+        <p className="text-xs pt-2 text-gray-400">{brand}</p>
+        <p className=" text-[15px]  text-black font-bold">{title}</p>
+        <div className="flex justify-start ">
+          <p className="text-xs text-green-500 pr-1">{price}$</p>{" "}
+          <p className="text-xs text-red-500 line-through ">
+            {discountPercentage}$
+          </p>
+        </div>
+        <div className="text-xs text-amber-400 pt-0.5 flex items-center ">
+          <Star width={12} height={12} />
+          <p className="pl-1">{rating}</p>
+        </div>
+      </div>
     </div>
   );
 };
--- React/ecommerce/src/Components/ProductGrid.jsx
+++ React/ecommerce/src/Components/ProductGrid.jsx
@@ -7,6 +7,7 @@ const ProductGrid = () => {
   async function getData() {
     let apiData = await fetch(`https://dummyjson.com/products?limit=30&skip=0`);
     let jsonData = await apiData.json();
+    console.log("Dtaa fetched");
     setProductData(jsonData.products);
   }
 
@@ -14,9 +15,9 @@ const ProductGrid = () => {
     getData();
   }, []);
   return (
-    <div>
+    <div className="flex justify-evenly w-screen min-h-screen flex-wrap gap-5 mt-7">
       {productData.map((pObj) => (
-        <ProductCard data={pObj} />
+        <ProductCard key={pObj.id} data={pObj} />
       ))}
     </div>
   );
