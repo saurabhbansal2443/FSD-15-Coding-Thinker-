ec386f94cccb0baa5ee31788298e900c8eb6a014
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2026-02-26 19:44:24 +0530

    Carousal icons

---
+++ React/ecommerce/src/Components/Carousal.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import image1 from "../Assets/image1";
+import image2 from "../Assets/image2";
+import image3 from "../Assets/image3";
+import image4 from "../Assets/image4";
+import ChevronLeft from "../Icons/ChevronLeft";
+import ChevronRight from "../Icons/ChevronRight";
+
+const Carousal = () => {
+  return <div></div>;
+};
+
+export default Carousal;
