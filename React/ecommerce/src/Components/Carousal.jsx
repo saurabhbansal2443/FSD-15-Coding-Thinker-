060bcc2bd2ca50a2d6d21199d9e58222f8751508
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2026-02-26 20:02:46 +0530

    Basic carousal

--- React/ecommerce/src/Components/Carousal.jsx
+++ React/ecommerce/src/Components/Carousal.jsx
@@ -1,13 +1,41 @@
-import React from "react";
-import image1 from "../Assets/image1";
-import image2 from "../Assets/image2";
-import image3 from "../Assets/image3";
-import image4 from "../Assets/image4";
+import React, { useState } from "react";
+import image1 from "../Assets/image1.webp";
+import image2 from "../Assets/image2.webp";
+import image3 from "../Assets/image3.webp";
+import image4 from "../Assets/image4.webp";
 import ChevronLeft from "../Icons/ChevronLeft";
 import ChevronRight from "../Icons/ChevronRight";
-
+let images = [image1, image2, image3, image4];
 const Carousal = () => {
-  return <div></div>;
+  const [activeIndex, setActiveIndex] = useState(1);
+
+  function handleLeft() {
+    setActiveIndex((activeIndex - 1 + images.length) % images.length);
+  }
+
+  function handleRight() {
+    setActiveIndex((activeIndex + 1) % images.length);
+  }
+
+  return (
+    <div className="h-[45vh] w-screen relative">
+      <div
+        onClick={handleLeft}
+        className="bg-white h-10 w-8 absolute left-0 flex justify-center items-center  top-[20vh] "
+      >
+        <ChevronLeft />
+      </div>
+      <div
+        onClick={handleRight}
+        className="bg-white h-10 w-8 absolute right-0 flex justify-center items-center  top-[20vh] "
+      >
+        <ChevronRight />
+      </div>
+      <div className="h-full w-full">
+        <img className="h-full w-full" src={images[activeIndex]} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default Carousal;
