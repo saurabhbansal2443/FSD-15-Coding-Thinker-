23973430886170425dda5b33cd5bb8438648ebea
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2026-03-18 20:12:43 +0530

    Home page optimized and cleaned up using custom hook

--- React/ecommerce/src/Components/ProductGrid.jsx
+++ React/ecommerce/src/Components/ProductGrid.jsx
@@ -1,53 +1,14 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useState } from "react";
 import ProductCard from "./ProductCard";
 import ProductCardSkeleton from "./ProductCardSkeleton";
 import { ThemeContext } from "../Store/ThemeProvider";
-import { useDispatch, useSelector } from "react-redux";
-import { addProductDataById } from "../app/ProductSlice";
-import { addProductsArrayByPage } from "../app/ProductSlice";
+import UseProducts from "../Hooks/UseProducts";
 
 const ProductGrid = () => {
-  const homePageMap = useSelector((store) => store.product.homePageMap);
   const { theme } = useContext(ThemeContext);
-  const [productData, setProductData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
-  const dispatch = useDispatch();
+  const { productData, loading, error } = UseProducts(currentPage);
 
-  async function getData() {
-    try {
-      let limit = 16;
-      let skip = (currentPage - 1) * limit;
-      let apiData = await fetch(
-        `https://dummyjson.com/products?limit=${limit}&skip=${skip}`,
-      );
-      let jsonData = await apiData.json();
-      setProductData(jsonData.products);
-      dispatch(
-        addProductsArrayByPage({
-          pageNumber: currentPage,
-          productArray: jsonData.products,
-        }),
-      );
-      dispatch(addProductDataById(jsonData.products));
-    } catch (err) {
-      console.log(err);
-      setError(true);
-    } finally {
-      setLoading(false);
-    }
-  }
-
-  useEffect(() => {
-    const cacheProductData = homePageMap[currentPage];
-    if (!cacheProductData) {
-      getData();
-    } else {
-      setProductData(cacheProductData);
-      setLoading(false);
-    }
-  }, [currentPage]);
   if (loading) {
     return <ProductCardSkeleton />;
   }
