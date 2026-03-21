414f04f019c66fdc7d7f9fc5daa86a44f3f69a59
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2026-03-21 16:08:11 +0530

    Wishlist ended

--- React/ecommerce/src/Screens/Wishlist.jsx
+++ React/ecommerce/src/Screens/Wishlist.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
-
+import { useSelector } from "react-redux";
+import ProductCard from "../Components/ProductCard";
 const Wishlist = () => {
+  const wishlistData = useSelector((store) => store.product.wishlistData);
+  const data = Object.values(wishlistData);
+
   return (
     <div>
       <Navbar />
+      {data.length > 0 ? (
+        <div className="flex justify-evenly w-screen min-h-screen flex-wrap gap-5 mt-100 z-10">
+          {data.map((pObj) => (
+            <ProductCard key={pObj.id} data={pObj} />
+          ))}
+        </div>
+      ) : (
+        <h1>No products in wishlist </h1>
+      )}
     </div>
   );
 };
