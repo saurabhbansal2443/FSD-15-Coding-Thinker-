3ada69a608c6564385898ff0fcf486242f112a9c
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2026-03-28 13:46:19 +0530

    UseMemo , useCallback, React.memo

--- React/class-components/src/App.jsx
+++ React/class-components/src/App.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    };
+  }
+
+  increment = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+  decrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
   render() {
-    return <h1> I am class component </h1>;
+    return (
+      <>
+        <h1> I am class component </h1>
+        <button onClick={this.increment}>PLUS</button>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.decrement}>MINUS</button>
+      </>
+    );
   }
 }
 
