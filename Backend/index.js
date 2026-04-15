6c31ebe1171c8162339469e027acb0348255caeb
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2026-04-15 20:30:16 +0530

    Server side rendering

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,31 @@
 import http from "http";
+import fs from "fs";
+import { json } from "stream/consumers";
+
+let productsData = fs.readFileSync("./Data.json", "utf-8");
+let jsonProductData = JSON.parse(productsData);
+let htmlFile = fs.readFileSync("./index.html", "utf-8");
 
 const server = http.createServer((req, res) => {
-  res.end("Hello");
+  console.log(req.url);
+  if (req.url == "/") {
+    res.setHeader("Content-type", "text/html");
+    res.end(htmlFile);
+  } else if (req.url == "/products") {
+    res.setHeader("Content-type", "text/json");
+    res.end(productsData);
+  } else if (req.url.startsWith("/products")) {
+    let array = req.url.split("/");
+    let index = array[array.length - 1];
+    let title = jsonProductData[index]?.title;
+    let description = jsonProductData[index]?.description;
+    res.end(
+      htmlFile
+        .replace("{{--tittle--}}", title)
+        .replace("{{--description--}}", description),
+    );
+  }
+  //
 });
 
 server.listen(8080, () => {
