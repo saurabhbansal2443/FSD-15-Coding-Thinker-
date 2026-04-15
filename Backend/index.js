55699fe4b4214e2b9e9f94ff56b371b2627aa99b
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2026-04-15 19:15:49 +0530

    backend satrted with server making and nodemon installed

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,8 @@
 import http from "http";
 
-const server = http.createServer((req, res) => {});
+const server = http.createServer((req, res) => {
+  res.end("Hello");
+});
 
 server.listen(8080, () => {
   console.log("server started");
