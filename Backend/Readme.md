3c274be924b876af486fbf4459097268d0bcad7a
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2026-04-21 19:00:33 +0530

    Express backend

---
+++ Backend/Readme.md
@@ -0,0 +1,11 @@
+1) Create the server using express  // complete 
+2) Create the Routes  // complete 
+3) Get the data  // complete 
+4) Write the logics of API 
+5) Deploy 
+
+API List 
+1) search 
+2) products?limit=20&skip=20
+3) Product/catgory
+4) product/id
--- Backend/index.js
+++ Backend/index.js
@@ -1,33 +1,29 @@
-import http from "http";
+import express from "express";
 import fs from "fs";
-import { json } from "stream/consumers";
 
-let productsData = fs.readFileSync("./Data.json", "utf-8");
-let jsonProductData = JSON.parse(productsData);
-let htmlFile = fs.readFileSync("./index.html", "utf-8");
+const productData = JSON.parse(fs.readFileSync("./Data.json", "utf-8"));
 
-const server = http.createServer((req, res) => {
-  console.log(req.url);
-  if (req.url == "/") {
-    res.setHeader("Content-type", "text/html");
-    res.end(htmlFile);
-  } else if (req.url == "/products") {
-    res.setHeader("Content-type", "text/json");
-    res.end(productsData);
-  } else if (req.url.startsWith("/products")) {
-    let array = req.url.split("/");
-    let index = array[array.length - 1];
-    let title = jsonProductData[index]?.title;
-    let description = jsonProductData[index]?.description;
-    res.end(
-      htmlFile
-        .replace("{{--tittle--}}", title)
-        .replace("{{--description--}}", description),
-    );
+const server = express();
+
+server.get("/products/:id", (req, res) => {
+  const id = req.params.id;
+  if (id <= 0 || id > productData.length) {
+    res.send({
+      product: null,
+      error: "Invalid product id",
+    });
+  } else {
+    const data = productData[id - 1];
+    res.send({
+      product: data,
+      error: null,
+    });
   }
-  //
 });
+server.get("/products/:search", (req, res) => {});
+server.get("/products", (req, res) => {}); // query params --> limit and skip
+server.get("/products/:catgeory", (req, res) => {});
 
 server.listen(8080, () => {
-  console.log("server started");
+  console.log("Server started");
 });
