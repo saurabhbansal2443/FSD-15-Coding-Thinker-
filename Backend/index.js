9c256a4345670f95a11ea70c9e67320daa048854
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2026-04-25 18:06:00 +0530

    Fronted Api ready

--- Backend/index.js
+++ Backend/index.js
@@ -20,9 +20,42 @@ server.get("/products/:id", (req, res) => {
     });
   }
 });
-server.get("/products/:search", (req, res) => {});
-server.get("/products", (req, res) => {}); // query params --> limit and skip
-server.get("/products/:catgeory", (req, res) => {});
+server.get("/products/search/:search", (req, res) => {
+  let searchQuery = req.params.search;
+
+  searchQuery = searchQuery.trim().toLowerCase();
+
+  const filteredArray = productData.filter((data) =>
+    data.title.trim().toLowerCase().includes(searchQuery),
+  );
+  res.send(filteredArray);
+});
+server.get("/products", (req, res) => {
+  const { skip, limit } = req.query;
+  if (skip && limit) {
+    const products = productData.slice(skip, Number(skip) + Number(limit));
+    res.send(products);
+  } else if (!skip) {
+    const products = productData.slice(0, limit);
+    res.send(products);
+  } else if (!limit) {
+    const products = productData.slice(skip, Number(skip) + 30);
+    res.send(products);
+  } else {
+    const products = productData.slice(0, 30);
+    res.send(products);
+  }
+}); // query params --> limit and skip
+server.get("/products/category/:category", (req, res) => {
+  let category = req.params.category;
+
+  category = category.trim().toLowerCase();
+
+  const filteredArray = productData.filter((data) =>
+    data.category.trim().toLowerCase().includes(category),
+  );
+  res.send(filteredArray);
+});
 
 server.listen(8080, () => {
   console.log("Server started");
