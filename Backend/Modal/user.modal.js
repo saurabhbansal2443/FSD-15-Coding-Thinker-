8a2fc6f07b99c433f13cd30991544549e0725f0a
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2026-04-30 18:52:08 +0530

    User Modal

---
+++ Backend/Modal/user.modal.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const { Schema, model } = mongoose;
+
+const addressSchema = new Schema({
+  city: String,
+  pincode: Number,
+  state: String,
+});
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "user's name not found"],
+      min: [3, "Name should contain at least 3 letter"],
+      max: [25, "Maximum length is 25 for name "],
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+    },
+    address: { type: [addressSchema], default: [] },
+  },
+  { timestamps: true },
+);
+
+const User = modal("Users", userSchema);
